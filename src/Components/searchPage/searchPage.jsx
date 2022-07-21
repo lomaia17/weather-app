880b611275ae9d7763cfc1817fcd20ada010aa79
Giorgi Lomaia <54961682+Lomaia17@users.noreply.github.com>
2022-07-21 09:27:12 +0400

    Added loader

--- src/Components/searchPage/searchPage.jsx
+++ src/Components/searchPage/searchPage.jsx
@@ -9,8 +9,10 @@ const SearchPage = () => {
 
   const [query, setQuery] = useState("");
   const [error, setError] = useState(null);
+  const [isLoading, setLoading] = useState(false);
   const { weather, setWeather } = useContext(WeatherContext);
   const getInputValue = () => {
+    setLoading(true);
     if (query) {
       fetch(`${api.base}forecast?q=${query}&units=metric&APPID=${api.key}`)
         .then((res) => res.json())
@@ -19,31 +21,57 @@ const SearchPage = () => {
           setQuery("");
         });
     }
+    setLoading(false);
   };
   return (
     <div>
       <div className="shadow-md rounded-xl mx-5 sm:px-8 sm:pt-6 sm:pb-8 sm:mb-4 sm:mt-4 bg-slate-800">
         <div className="pt-8 pb-10 pr-20 pl-20">
           <p className="text-white text-center">Search For Location</p>
-          <form>
-            <input
-              className={`border appearance-none  block w-full mt-5 bg-gray-200 text-gray-700  rounded-lg py-2 px-4 mb-3 leading-tight focus:outline-none focus:bg-white ${
-                error && "border-red-600"
-              }`}
-              id="grid-first-name"
-              type="text"
-              placeholder="City"
-              onChange={(e) => setQuery(e.target.value)}
-            />
-            {error && <p className="text-red-500 text-s">{error}</p>}
-            <button
-              class="mt-2 bg-gradient-to-r from-cyan-500 to-blue-500 w-full hover:bg-teal-700 border-none text-sm border-4 text-white py-2 px-2 rounded-xl"
-              type="button"
-              onClick={getInputValue}
+          {isLoading ? (
+            <div
+              className="flex items-center justify-center mt-8"
+              role="status"
             >
-              Search
-            </button>
-          </form>
+              <svg
+                aria-hidden="true"
+                class="mr-2 w-8 h-8 text-gray-200 animate-spin dark:text-slate-50 fill-sky-500"
+                viewBox="0 0 100 101"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                  fill="currentColor"
+                />
+                <path
+                  d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                  fill="currentFill"
+                />
+              </svg>
+              <span class="sr-only">Loading...</span>
+            </div>
+          ) : (
+            <form>
+              <input
+                className={`border appearance-none  block w-full mt-5 bg-gray-200 text-gray-700  rounded-lg py-2 px-4 mb-3 leading-tight focus:outline-none focus:bg-white ${
+                  error && "border-red-600"
+                }`}
+                id="grid-first-name"
+                type="text"
+                placeholder="City"
+                onChange={(e) => setQuery(e.target.value)}
+              />
+              {error && <p className="text-red-500 text-s">{error}</p>}
+              <button
+                class="mt-2 bg-gradient-to-r from-cyan-500 to-blue-500 w-full hover:bg-teal-700 border-none text-sm border-4 text-white py-2 px-2 rounded-xl"
+                type="button"
+                onClick={getInputValue}
+              >
+                Search
+              </button>
+            </form>
+          )}
         </div>
       </div>
     </div>
